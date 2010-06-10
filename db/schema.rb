1161513add71399276f00c5eef9bd0a6deb0fee2
Noah Gibbs <noah_gibbs@yahoo.com>
2010-06-09 22:04:23 -0700

    Added scaffold for snippets

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100609054429) do
+ActiveRecord::Schema.define(:version => 20100610045416) do
 
   create_table "snippets", :force => true do |t|
     t.string   "title"
