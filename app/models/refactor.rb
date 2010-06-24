4ce9bd6b06f44aee78e2e307b3f7bd0ccb1f7ff9
Noah Gibbs <noah_gibbs@yahoo.com>
2010-06-23 18:24:08 -0700

    Do verification before save and import Language constants

--- app/models/refactor.rb
+++ app/models/refactor.rb
@@ -1,4 +1,11 @@
 class Refactor < ActiveRecord::Base
   belongs_to :snippet
   has_many :votes
+
+  Languages = Snippet::Languages
+
+  def before_save
+    self.body = self.body.strip
+    self.language = Languages[self.language] || "Ruby"
+  end
 end
