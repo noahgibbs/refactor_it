f93142e06c23fc9795a613956258b57fd97bafbd
Noah Gibbs <noah_gibbs@yahoo.com>
2010-06-23 20:04:24 -0700

    Make karma default to 0

--- db/migrate/20100624022410_add_karma_to_snippets.rb
+++ db/migrate/20100624022410_add_karma_to_snippets.rb
@@ -1,9 +1,9 @@
 class AddKarmaToSnippets < ActiveRecord::Migration
   def self.up
-    add_column :snippets, :karma, :integer
+    add_column :snippets, :karma, :integer, :default => 0
   end
 
   def self.down
-    remove_column :snippets, :karma
+    remove_column :snippets, :karma, :default => 0
   end
 end
--- db/migrate/20100624022455_add_karma_to_refactors.rb
+++ db/migrate/20100624022455_add_karma_to_refactors.rb
@@ -1,9 +1,9 @@
 class AddKarmaToRefactors < ActiveRecord::Migration
   def self.up
-    add_column :refactors, :karma, :integer
+    add_column :refactors, :karma, :integer, :default => 0
   end
 
   def self.down
-    remove_column :refactors, :karma
+    remove_column :refactors, :karma, :default => 0
   end
 end
